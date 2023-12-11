69e4788948a2fd16b2141f844975bebb365d8af4
tikhneva <134695603+tikhneva@users.noreply.github.com>
2023-12-11 08:00:51 +0200

    дерево дз.cpp

--- дерево дз/дерево дз.cpp
+++ дерево дз/дерево дз.cpp
@@ -1,7 +1,38 @@
 ﻿#include <iostream>
 #include <set>
-
 using namespace std;
+
+//////////int
+
+int main()
+{
+    set<int> Tree;
+
+    Tree.insert(14);
+    Tree.insert(10);
+    Tree.insert(9);
+    Tree.insert(27);
+    Tree.insert(21);
+
+    cout << "All tree elements:\n";
+    for (const int& elements : Tree)
+    {
+        cout << elements << "\n";
+    }
+
+    Tree.erase(10);
+    Tree.erase(27);
+
+    cout << "\nTree elements after removal:\n";
+    for (const int& elements : Tree)
+    {
+        cout << elements << "\n";
+    }
+
+    return 0;
+}
+
+//////////string
 
 int main()
 {
