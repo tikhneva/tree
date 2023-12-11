316acc40e7736466a3fa9b11d9d7338080a0c2ac
Milana <mukanmilana@gmail.com>
2023-12-11 07:50:27 +0200

    Add project files.

---
+++ дерево дз/дерево дз.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<string> Tree;
+
+    Tree.insert("kimchi");
+    Tree.insert("ramyeon");
+    Tree.insert("bulgogi");
+    Tree.insert("bibimbap");
+    Tree.insert("samgyeopsal");
+
+    cout << "All tree elements:\n";
+    for (const string& elements : Tree) 
+    {
+        cout << elements << "\n";
+    }
+
+    Tree.erase("ramyeon");
+    Tree.erase("bibimbap");
+
+    cout << "\nTree elements after removal:\n";
+    for (const string& elements : Tree)
+    {
+        cout << elements << "\n";
+    }
+
+    return 0;
+}
